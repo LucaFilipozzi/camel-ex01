c12df33c80c4659985540bc06c5aacaad7e7a2d0
Luca Filipozzi <luca.filipozzi@gmail.com>
2024-03-22 20:32:03 -0700

    try caffeine cache

--- src/main/java/com/github/lucafilipozzi/camel/ex01/MyRouteBuilder.java
+++ src/main/java/com/github/lucafilipozzi/camel/ex01/MyRouteBuilder.java
@@ -2,18 +2,29 @@
 
 package com.github.lucafilipozzi.camel.ex01;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.apache.camel.builder.RouteBuilder;
 
 public class MyRouteBuilder extends RouteBuilder {
 
     public void configure() {
+        getContext().getRegistry().bind("cache", Caffeine.newBuilder().build());
+
         from("file:src/data?noop=true")
             .choice()
                 .when(xpath("/person/city = 'London'"))
                     .log("UK message")
+                    .to("direct:start")
                     .to("file:target/messages/uk")
                 .otherwise()
                     .log("Other message")
+                    .to("direct:start")
                     .to("file:target/messages/others");
+
+        from("direct:start")
+            .to("caffeine-cache://cache?action=PUT&key=1")
+            .to("caffeine-cache://cache?action=GET&key=1")
+            .log("test: ${body}")
+            .to("mock:result");
     }
 }
