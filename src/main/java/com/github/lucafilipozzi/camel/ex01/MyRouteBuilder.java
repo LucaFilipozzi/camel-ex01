82ef5851cf11211cc41dac0541e2b2e14111bbe9
Luca Filipozzi <luca.filipozzi@gmail.com>
2024-03-24 13:59:52 -0700

    make SonarLint happy

--- src/main/java/com/github/lucafilipozzi/camel/ex01/MyRouteBuilder.java
+++ src/main/java/com/github/lucafilipozzi/camel/ex01/MyRouteBuilder.java
@@ -2,12 +2,23 @@
 
 package com.github.lucafilipozzi.camel.ex01;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.support.SimpleUuidGenerator;
-import org.apache.camel.CamelContext;
+import com.github.benmanes.caffeine.cache.Caffeine;
 
 public class MyRouteBuilder extends RouteBuilder {
+
+    private static final String PROCESSING_LOG_MESSAGE = "${id}: processing";
+    private static final String WHEN_RESULT_LOG_MESSAGE = "${id}: when result";
+    private static final String OTHERWISE_RESULT_LOG_MESSAGE = "${id}: otherwise result";
+    private static final String MOCK_RESULT = "mock:result";
+    private static final String DIRECT_XML = "direct:xml";
+    private static final String DIRECT_XPATH = "direct:xpath";
+    private static final String DIRECT_XQUERY = "direct:xquery";
+    private static final String DIRECT_JSON = "direct:json";
+    private static final String DIRECT_JSONPATH = "direct:jsonpath";
+    private static final String DIRECT_JQ = "direct:jq";
 
     public void configure() {
         CamelContext camelContext = getCamelContext();
@@ -17,80 +28,80 @@ public class MyRouteBuilder extends RouteBuilder {
 
         // https://camel.apache.org/components/4.4.x/timer-component.html
         from("timer:trigger?repeatCount=5&fixedRate=true")
-            .routeId("trigger")
+            .routeId("timer:trigger")
             .log("hi mum")
-            .to("direct:cache");
+            .to(MOCK_RESULT);
 
         // https://camel.apache.org/components/4.4.x/file-component.html
-        // https://camel.apache.org/components/4.4.x/xj-component.html
         from("file:src/data?noop=true")
-            .routeId("mainflow")
-            .to("direct:xmlHandler")
-            .to("xj:identity?transformDirection=XML2JSON")
-            .to("direct:jsonHandler");
+            .routeId("file:src/data")
+            .to(DIRECT_XML)
+            .to(DIRECT_JSON);
 
-        from("direct:xmlHandler")
-            .routeId("xmlHandler")
-            .to("direct:xmlflow1")  // xmlflow1 processes xml using xpath
-            .to("direct:xmlflow2"); // xmlflow2 processes xml using xquery
+        from(DIRECT_XML)
+            .routeId(DIRECT_XML)
+            .to(DIRECT_XPATH)
+            .to(DIRECT_XQUERY);
 
-        from("direct:jsonHandler")
-            .routeId("jsonHandler")
-            .to("direct:jsonflow1")  // jsonflow1 processes json using jq
-            .to("direct:jsonflow2"); // jsonflow2 processes json using jsonpath
+        // https://camel.apache.org/components/4.4.x/xj-component.html
+        from(DIRECT_JSON)
+            .routeId(DIRECT_JSON)
+            .to("xj:identity?transformDirection=XML2JSON&failOnNullBody=true")
+            .to(DIRECT_JQ)
+            .to(DIRECT_JSONPATH);
 
         // https://camel.apache.org/components/4.4.x/languages/xpath-language.html
-        from("direct:xmlflow1")
-            .routeId("xmlflow1")
-            .log("${id}: processing")
+        from(DIRECT_XPATH)
+            .routeId(DIRECT_XPATH)
+            .log(PROCESSING_LOG_MESSAGE)
             .choice()
                 .when().xpath("/person/city = 'London'")
-                    .log("${id}: UK message")
-                    .to("file:target/messages/uk")
+                    .log(WHEN_RESULT_LOG_MESSAGE)
+                    .to(MOCK_RESULT)
                 .otherwise()
-                    .log("${id}: Other message")
-                    .to("file:target/messages/others");
+                    .log(OTHERWISE_RESULT_LOG_MESSAGE)
+                    .to(MOCK_RESULT);
 
         // https://camel.apache.org/components/4.4.x/languages/xquery-language.html
-        from("direct:xmlflow2")
-            .routeId("xmlflow2")
-            .log("${id}: processing")
+        from(DIRECT_XQUERY)
+            .routeId(DIRECT_XQUERY)
+            .log(PROCESSING_LOG_MESSAGE)
             .choice()
                 .when().xquery("/person[city = 'London']")
-                    .log("${id}: UK message")
-                    .to("file:target/messages/uk")
+                    .log(WHEN_RESULT_LOG_MESSAGE)
+                    .to(MOCK_RESULT)
                 .otherwise()
-                    .log("${id}: Other message")
-                    .to("file:target/messages/others");
+                    .log(OTHERWISE_RESULT_LOG_MESSAGE)
+                    .to(MOCK_RESULT);
 
         // https://camel.apache.org/components/4.4.x/languages/jq-language.html
-        from("direct:jsonflow1")
-            .routeId("jsonflow1")
-            .log("${id}: processing")
+        from(DIRECT_JQ)
+            .routeId(DIRECT_JQ)
+            .log(PROCESSING_LOG_MESSAGE)
             .choice()
                 .when().jq(".city == \"London\"")
-                    .log("${id}: UK message")
-                    .to("mock:result")
+                    .log(WHEN_RESULT_LOG_MESSAGE)
+                    .to(MOCK_RESULT)
                 .otherwise()
-                    .log("${id}: Other message")
-                    .to("mock:result");
+                    .log(OTHERWISE_RESULT_LOG_MESSAGE)
+                    .to(MOCK_RESULT);
 
         // https://camel.apache.org/components/4.4.x/languages/jsonpath-language.html
-        from("direct:jsonflow2")
-            .routeId("jsonflow2")
-            .log("${id}: processing")
+        from(DIRECT_JSONPATH)
+            .routeId(DIRECT_JSONPATH)
+            .log(PROCESSING_LOG_MESSAGE)
             .choice()
                 .when().jsonpath("$[?(@.city == 'London')]")
-                    .log("${id}: UK message")
-                    .to("mock:result")
+                    .log(WHEN_RESULT_LOG_MESSAGE)
+                    .to(MOCK_RESULT)
                 .otherwise()
-                    .log("${id}: Other message")
-                    .to("mock:result");
+                    .log(OTHERWISE_RESULT_LOG_MESSAGE)
+                    .to(MOCK_RESULT);
 
         from("direct:cache")
-            .routeId("cache")
+            .routeId("direct:cache")
             .to("caffeine-cache://cache?action=PUT&key=1")
             .to("caffeine-cache://cache?action=GET&key=1")
-            .to("mock:result");
+            .to(MOCK_RESULT);
     }
 }
